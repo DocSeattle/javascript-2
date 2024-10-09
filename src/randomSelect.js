59cbc7bfdf4c922fe19890ee52b5a54247956865
DocSeattle <seattledraws@gmail.com>
2024-10-09 11:27:59 +0200

    added task source

---
+++ src/randomSelect.js
@@ -0,0 +1,4 @@
+function randomFromArray(arr){
+    //TODO: Your code here to select a random element from the array passed to the function.
+  }
+    //TODO: Export the randomFromArray with name randomSelect function.
